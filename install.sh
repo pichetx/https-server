9ffef60e4b6f6dceb1df068defc19caba4ecc459
pichet <98628678+pichetx@users.noreply.github.com>
2023-07-21 00:05:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@ pkg install python3 -y
 pkg install figlet -y
 
 chmod +x run-node
+chmod +x edit-miner
 
 mv run-node /data/data/com.termux/files/usr/bin
+mv edit-miner /data/data/com.termux/files/usr/bin
 
 cd /data/data/com.termux/files/usr/bin
 
 chmod +x run-node
+chmod +x edit-miner
 
 cd && cd /data/data/com.termux/files/usr/etc
 nano bash.bashrc
