15eb354ee3525a14e2c9f0fc4cfbdd80cc5f30fd
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-02 03:39:22 +0100

    Update editserver.py

--- editserver.py
+++ editserver.py
@@ -70,21 +70,20 @@ while True:
     banner()
     setminer()
     with open("online.json",encoding="utf-8") as set:
-        	load = set.read()
-        	loads = json.loads(load)
-        	pool = loads['pool']
-        	wallet = loads['wallet']
-        	password = loads['pass']
+        load = set.read()
+        loads = json.loads(load)
+        pool = loads['pool']
+        wallet = loads['wallet']
+        password = loads['pass']
         	
-        	print("\033[36m")
-        	print("การตั้งค่าที่บันทึก")
-        	print("POOL  =",pool)
-        	print("WALLET=",wallet)
-        	print("PASS  =",password)
-        	print("\033[0m\n")
-        	print("\033[31mโปรดตรวจสอบการตั้งค่า ถ้าถูกต้อง ให้ใช้คำสั่ง  run-node  หรือ edit-miner เพื่อแก้ไข\033[0m")
+        print("\033[36m")
+        print("การตั้งค่าที่บันทึก")
+        print("POOL  =",pool)
+        print("WALLET=",wallet)
+        print("PASS  =",password)
+        print("\033[0m\n")
+        print("\033[31mโปรดตรวจสอบการตั้งค่า ถ้าถูกต้อง ให้ใช้คำสั่ง  run-node  หรือ edit-miner เพื่อแก้ไข\033[0m")
     break
-
 
 
 
